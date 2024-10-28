59592c88b84b3e358da9987dbfecfcb5c1aa1436
Mohamed Elazhary <mmalazhari@users.noreply.github.com>
2024-10-28 15:07:09 +0200

    mern-auth-office

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Header from "./components/Header"
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+        <Routes>
+            <Rout></Rout>
+        </Routes>
+    </BrowserRouter>
+  )
+}
